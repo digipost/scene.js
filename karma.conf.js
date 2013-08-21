9697e7069283aa37571da67340e633a5882f2994
olav <olav@bjorkoy.com>
2013-08-21 14:07:36 +0200

    Upgrade karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -1,57 +1,31 @@
 // Karma configuration
+// Generated on Mon Aug 19 2013 09:03:59 GMT+0200 (CEST)
 
-// base path, that will be used to resolve files and exclude
-basePath = '.';
+module.exports = function(config) {
+  config.set({
 
-// list of files / patterns to load in the browser
-files = [
-  MOCHA,
-  MOCHA_ADAPTER,
+    basePath: '.',
+    frameworks: ['mocha'],
+    preprocessors: {},
 
-  { pattern: 'spec/templates/*.html', watched: true, included: false, served: true },
+    files: [
+        { pattern: 'spec/templates/*.html', watched: true, included: false, served: true },
+        'spec/vendor/chai.js',
+        'spec/vendor/sinon-chai.js',
+        'spec/vendor/*.js',
+        'scene.js',
+        'spec/helpers.js',
+        'spec/*.js'
+    ],
 
-  'spec/vendor/chai.js',
-  'spec/vendor/sinon-chai.js',
-  'spec/vendor/*.js',
-  'scene.js',
-  'spec/helpers.js',
-  'spec/*.js'
-];
-
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
-
-// web server port
-port = 9876;
-
-// cli runner port
-runnerPort = 9100;
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
-
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_WARN;
-
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
-
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-// - scripts/phantom.sh
-browsers = ['PhantomJS'];
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    exclude: [],
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_WARN,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    captureTimeout: 60000,
+    singleRun: false
+  });
+};
